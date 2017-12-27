78f6240d7b3f4548262c736dc08994b8d96861a5
CarlosMellaN <carlosmellaneira@gmail.com>
2017-12-27 10:08:14 -0300

    contacto
    prueba

--- contacto.php
+++ contacto.php
@@ -6,15 +6,19 @@
     </div>
     <div class="contenedor-mapa-form">
     	<div class="formulario">
-    		<label for="Nombre">Nombre</label>
-    		<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nombre completo">
-    		<label for="Email">Email</label>
-    		<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="ejemple@correo.com">
-    		<label for="Teléfono">Teléfono</label>
-    		<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="987654321">
-    		<label for="texto">texto:</label>
-  			<textarea class="form-control" rows="5" id="texto"></textarea
-    		<p></p>
+            <form action="http://pruebabrother.96.lt/clinica_mc/envioconsulta.php" enctype="" method="post">
+                <label for="Nombre">Nombre</label>
+                <input type="text" name="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nombre completo" required>
+                <label for="Email">Email</label>
+                <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="ejemple@correo.com" required>
+                <label for="Teléfono">Teléfono</label>
+                <input type="text" name="telefono" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="987654321" required>
+                <label for="texto">texto:</label>
+                <textarea name="content" class="form-control" rows="5" id="texto" placeholder="Ingrese el contenido de su mensaje" required></textarea>
+                <p></p>
+                <input type="submit" value="Enviar" />
+            </form>
+    		
     	</div>
     	<div class="map"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3329.420818200103!2d-70.62065538480086!3d-33.43834128077744!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662cf7f06cdc26f%3A0xa3567bcce95e48bf!2sJos%C3%A9+Miguel+Claro+988%2C+Providencia%2C+Regi%C3%B3n+Metropolitana!5e0!3m2!1ses!2scl!4v1513868823298" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe></div>
     </div>
