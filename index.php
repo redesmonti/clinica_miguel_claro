f81ea831d4eb5653d3c60cb6ca8d38965ebe71d2
CarlosMellaN <carlosmellaneira@gmail.com>
2018-01-02 13:14:04 -0300

    entradas
    style.css modificado

--- index.php
+++ index.php
@@ -3,9 +3,13 @@
 <?php if ( have_posts() ) : the_post(); ?>
   <section>
   	<div class="container">
-  		<h1 class="contenedor-entradas"><?php the_title(); ?></h1>
-	    <?php  if ( has_post_thumbnail() ) { the_post_thumbnail('medium', array('class' => 'img-responsive')); }?>
-	    <?php the_content(); ?>
+  		<div class="contenedor-entradas">
+  			<div class="titulo">
+  				<h1><?php the_title(); ?></h1>
+			    <?php  if ( has_post_thumbnail() ) { the_post_thumbnail('medium', array('class' => 'img-responsive')); }?>
+			    <p><?php the_content(); ?></p>
+  			</div>
+  		</div>
   	</div>
   </section>
 <?php endif; ?>
