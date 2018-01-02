484b53234c984bc2d280c7a1ee9be2f417726b27
CarlosMellaN <carlosmellaneira@gmail.com>
2018-01-02 12:49:07 -0300

    footer
    foter

--- footer.php
+++ footer.php
@@ -1,7 +1,30 @@
-		<footer>
-			<small> <?php echo date("Y") ?></small>
-			 
+
+		<footer id="myFooter" class="fixed-bottom">
+	        <div class="container">
+	            <div class="row">
+	                <div class="col-sm-3">
+	                    <img class="img-footer" src="<?php echo get_template_directory_uri(); ?>/assets/img/logo_miguelclaro.png" alt="">
+	                </div>
+	                <div class="col-sm-3">
+	                    <h5>Clinica Miguel Claro</h5>
+	                    <ul>
+	                        <li>Miguel Claro 988 - Providencia -Santiago</li>
+	                        <li>Fono: (56 2) 27140600</li>
+	                        <li>Contamos con estacionamientos gratuitos</li>
+	                    </ul>
+	                </div>
+	            </div>
+	        </div>
+	        <div class="second-bar">
+	           <div class="container">
+	                <div class="social-icons">
+	                    <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
+	                    <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
+	                    <a href="#" class="google"><i class="fa fa-google-plus"></i></a>
+	                </div>
+	            </div>
+        	</div>
+			<?php wp_footer(); ?>
 		</footer>
-		<?php wp_footer(); ?>
 	</body>
 </hmtl>
